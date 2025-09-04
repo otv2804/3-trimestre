01b104186b2c07f7e1f084648230c02be8d04c36
otv2804 <otavio.raizer.fernandes@escola.pr.gov.br>
2025-09-04 09:00:06 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,11 +1,13 @@
 //muda o nome do titulo e do subtitulo
-var pacientes = document.querySelector(".subtitulo");
-pacientes.textContent = "Meus Pacientes";
+var subtitulo = document.querySelector(".subtitulo");
+subtitulo.textContent = "Meus Pacientes";
 var cabeçalho = document.querySelector(".titulo");
 cabeçalho.textContent = "Raizer Nutrição";
 
 //acessando a tag tr de Paulo
-var paciente = document.querySelector("#primeiro_paciente");
+var paciente = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < paciente.length; i = i++){
 
 //acessando o peso e a altura
 var tdPeso = paciente.querySelector(".info-peso");
@@ -13,15 +15,29 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-
-//acessando e fazendo o imc
+//imc conta
 var imc = peso / (altura * altura);
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
 
-if(peso < 0 || peso > 1000 ){
-    alert("Peso Inválido")
+//Variaveis com valor true
+var pesoValido = true;
+var alturaValido = true; 
+
+if(pesoValido && alturaValido){
+    //acessando o imc
+    var tdImc = paciente.querySelector(".info-imc");
+    tdImc.textContent = imc;
+}
+
+//define se o peso ou altura está invalida
+if(peso <= 0 || peso >= 1000 ){
+    console.log("Peso Inválido");
+    var pesoValido = false;
+    tdImc.textContent = "Peso Inválido";
 };
-if(altura < 0 || altura > 3.00){
-    alert("Altura Inválida")
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura Inválida");
+    var alturaValido = false;
+    tdImc.textContent = "Altura Inválida";
 };
+
+}
